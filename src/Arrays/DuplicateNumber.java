9153c0cbaf7d14af2dd93ea1b9fb314172302a4d
nrashmi06 <rashminattoji24@gmail.com>
2024-07-14 23:48:45 +0530

    287. Find the Duplicate Number

---
+++ src/Arrays/DuplicateNumber.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class DuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != i + 1) {
+                int correct = nums[i] - 1;
+                if (nums[i] != nums[correct]) {
+                    swap(nums, i, correct);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return 0;
+    }
+
+    void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    public static void main(String[] args) {
+        DuplicateNumber duplicateNumber = new DuplicateNumber();
+        int[] nums = {1, 3, 4, 2, 2};
+        System.out.println(duplicateNumber.findDuplicate(nums));
+    }
+}
