e93be497e0e43caa328de48c0f58f3f3f5823926
nrashmi06 <rashminattoji24@gmail.com>
2024-07-14 23:50:56 +0530

    73. Set Matrix Zeroes

---
+++ src/Arrays/SetMatrixZeros.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+public class SetMatrixZeros {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SetMatrixZeros setMatrixZeros = new SetMatrixZeros();
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+        setMatrixZeros.setZeroes(matrix);
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
