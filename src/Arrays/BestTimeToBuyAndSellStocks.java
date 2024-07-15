548afa4fea637d6ceca355eaeb467901061e959e
nrashmi06 <rashminattoji24@gmail.com>
2024-07-15 21:54:05 +0530

    121. Best Time to Buy and Sell Stock

---
+++ src/Arrays/BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class BestTimeToBuyAndSellStocks {
+    public static int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else if (prices[i] - min > profit) {
+                profit = prices[i] - min;
+            }
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+}
