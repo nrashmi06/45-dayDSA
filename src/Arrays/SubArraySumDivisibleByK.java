c23aba0f68a5fb84ff0f5037deb6d1bf4b89d4fb
nrashmi06 <rashminattoji24@gmail.com>
2024-07-16 02:31:25 +0530

    974. Subarray Sums Divisible by K

---
+++ src/Arrays/SubArraySumDivisibleByK.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class SubArraySumDivisibleByK {
+    public static int subarraysDivByK(int[] nums, int k){
+        int arr[]=new int [k];
+        int sum=0;
+        int count=0;
+        arr[0]=1;
+        for (int i:nums){
+            sum=(i+sum)%k;
+            if (sum<0){
+                sum+=k;
+            }
+            count += arr[sum];
+            arr[sum]++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,0,-2,-3,1};
+        int k = 5;
+        System.out.println(subarraysDivByK(nums, k));
+    }
+
+}
