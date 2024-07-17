c222c21127b96b83f14da67b67a7c3b850cc934d
nrashmi06 <rashminattoji24@gmail.com>
2024-07-17 19:24:06 +0530

    55. Jump Game

---
+++ src/Arrays/JumpGame.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(canJump(nums));
+    }
+}
