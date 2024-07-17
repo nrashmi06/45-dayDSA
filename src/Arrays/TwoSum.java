bf9a29758afd1098b57b4c380adc33d9f8f5d7c5
nrashmi06 <rashminattoji24@gmail.com>
2024-07-17 16:05:55 +0530

    1. Two Sum

---
+++ src/Arrays/TwoSum.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+}
