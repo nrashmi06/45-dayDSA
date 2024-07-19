f5f77f08f4303e71cd10a1df65ec1519a3ed6876
nrashmi06 <rashminattoji24@gmail.com>
2024-07-19 18:03:04 +0530

    Print all the duplicate characters in a string gfg

---
+++ src/Strings/DuplicatesInString.java
@@ -0,0 +1,30 @@
+package Strings;/*package whatever //do not write package name here */
+import java.io.*;
+import java.util.*;
+
+class DuplicatesInString
+{
+
+    // Java program to count all duplicates
+    // from string using maps
+    static void printDups(String str)
+    {
+        Map<Character, Integer> count = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if(count.containsKey(str.charAt(i)))
+                count.put(str.charAt(i) , count.get(str.charAt(i))+1);
+            else count.put(str.charAt(i),1);
+
+        }
+
+        for (Map.Entry<Character,Integer> mapElement : count.entrySet()) {
+            if (mapElement.getValue() > 1)
+                System.out.println(mapElement.getKey() + ", count = " + mapElement.getValue());
+        }
+    }
+    public static void main(String args[])
+    {
+        String str = "test string";
+        printDups(str);
+    }
+}
