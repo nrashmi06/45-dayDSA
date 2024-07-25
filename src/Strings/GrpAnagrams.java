b032d225d3d88f267149e0647a0bf08ef18af604
nrashmi06 <rashminattoji24@gmail.com>
2024-07-26 01:33:50 +0530

    49. Group Anagrams

---
+++ src/Strings/GrpAnagrams.java
@@ -0,0 +1,31 @@
+package Strings;
+
+public class GrpAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+
+            map.get(sortedStr).add(str);
+        }
+
+        for (List<String> list : map.values()) {
+            result.add(list);
+        }
+
+        return result;
+    }
+}
